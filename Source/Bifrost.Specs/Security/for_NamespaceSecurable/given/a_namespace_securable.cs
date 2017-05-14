56f98c5adfe1efa2dbae9dcd18d80a994c35bcee
Einar Ingebrigtsen <einar@dolittle.com>
2017-05-14 11:19:56 -0700

    Fixing types used in specs + removing redunant spec

--- Source/Bifrost.Specs/Security/for_NamespaceSecurable/given/a_namespace_securable.cs
+++ Source/Bifrost.Specs/Security/for_NamespaceSecurable/given/a_namespace_securable.cs
@@ -7,17 +7,15 @@ namespace Bifrost.Specs.Security.for_NamespaceSecurable.given
     public class a_namespace_securable
     {
         protected static NamespaceSecurable namespace_securable;
-        protected static object action_with_exact_namespace_match;
-        protected static object action_with_partial_namespace_match;
+        protected static object action_with_namespace_match;
         protected static object action_within_another_namespace;
 
         public a_namespace_securable()
         {
-            action_with_exact_namespace_match = "A String";
-            action_with_partial_namespace_match = new SimpleCommand();
+            action_with_namespace_match = new SimpleCommand();
             action_within_another_namespace = new CommandInADifferentNamespace();
 
-            namespace_securable = new NamespaceSecurable(typeof(string).Namespace);
+            namespace_securable = new NamespaceSecurable(typeof(SimpleCommand).Namespace);
         }
     }
 }
