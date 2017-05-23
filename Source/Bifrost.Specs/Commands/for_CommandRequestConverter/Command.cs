c875b81a446216b759f70e5ae179160348bc4645
Einar Ingebrigtsen <einar@dolittle.com>
2017-05-23 15:15:54 +0200

    Adding some specs for the basics for CommandRequestConverter

---
+++ Source/Bifrost.Specs/Commands/for_CommandRequestConverter/Command.cs
@@ -0,0 +1,10 @@
+﻿using Bifrost.Commands;
+
+namespace Bifrost.Specs.Commands.for_CommandRequestConverter
+{
+    public class Command : ICommand
+    {
+        public string   StringProperty { get; set; }
+        public int IntProperty { get; set; }
+    }
+}
