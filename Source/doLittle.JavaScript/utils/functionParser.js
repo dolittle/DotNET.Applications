2fe08a7537b657bc7ce7d99580271f4dfea5b5d3
Einar Ingebrigtsen <einar@dolittle.com>
2017-05-30 21:59:31 +0200

    Matching named functions as well

--- Source/doLittle.JavaScript/utils/functionParser.js
+++ Source/doLittle.JavaScript/utils/functionParser.js
@@ -3,7 +3,7 @@ doLittle.namespace("doLittle", {
         parse: function(func) {
             var result = [];
 
-            var match = func.toString().match(/function\w*\s*\((.*?)\)/);
+            var match = func.toString().match(/function\s*\w*\((.*?)\)/);
             if (match !== null) {
                 var functionArguments = match[1].split(/\s*,\s*/);
                 functionArguments.forEach(function (item) {
