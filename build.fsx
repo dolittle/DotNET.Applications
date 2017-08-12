2a699bacdad3ec41f889aad442393b178b8dbdca
Einar Ingebrigtsen <einar@dolittle.com>
2017-08-12 20:43:05 +0200

    forcing netstandard 1.6

--- build.fsx
+++ build.fsx
@@ -254,7 +254,8 @@ Target "Build" (fun _ ->
     trace "**** Building ****"
     for directory in projectsDirectories do
         tracef "Building %s" directory.FullName
-        let allArgs = sprintf "build %s %s" directory.FullName (if isWindows then "" else "-f netstandard1.6")
+        //let allArgs = sprintf "build %s %s" directory.FullName (if isWindows then "" else "-f netstandard1.6")
+        let allArgs = sprintf "build %s %s" directory.FullName "-f netstandard1.6")
         spawnProcess("dotnet", allArgs)
 
     trace "**** Building Done ****"
