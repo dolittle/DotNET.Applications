eea85e89d63ef6e6f3c00f6be0f4e32f66b0ce60
Einar Ingebrigtsen <einar@dolittle.com>
2017-08-13 12:06:26 +0200

    Removing views spec

--- Source/doLittle.Specs/Configuration/for_Configure/when_initializing.cs
+++ Source/doLittle.Specs/Configuration/for_Configure/when_initializing.cs
@@ -9,7 +9,6 @@ namespace doLittle.Specs.Configuration.for_Configure
         Because of = () => configure_instance.Initialize();
 
         It should_call_initialize_on_events_configuration = () => events_configuration.Verify(e => e.Initialize(configure_instance.Container), Times.Once());
-        It should_call_initialize_on_views_configuration = () => views_configuration.Verify(e => e.Initialize(configure_instance.Container), Times.Once());
         It should_call_initialize_on_serialization_configuration = () => serialization_configuration.Verify(e => e.Initialize(configure_instance.Container), Times.Once());
         It should_call_initialize_on_commands_configuratuibn = () => commands_configuration.Verify(c => c.Initialize(configure_instance.Container), Times.Once());
         It should_call_initialize_on_default_storage = () => default_storage_configuration.Verify(d => d.Initialize(configure_instance.Container), Times.Once());
