e22981ebd9378327e459a2c416b9f961806f3b2c
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-14 09:46:56 +0100

    Whitespace be gone

--- Specifications/Artifacts/for_ArtifactTypeToTypeMaps.cs/when_mapping_to_artifact_type_with_existing_map.cs
+++ Specifications/Artifacts/for_ArtifactTypeToTypeMaps.cs/when_mapping_to_artifact_type_with_existing_map.cs
@@ -3,7 +3,6 @@ using Machine.Specifications;
 
 namespace Dolittle.Artifacts.for_ArtifactTypeToTypeMaps
 {
-
     public class when_mapping_to_artifact_type_with_existing_map : given.one_map
     {
         static IArtifactType result;
@@ -11,6 +10,5 @@ namespace Dolittle.Artifacts.for_ArtifactTypeToTypeMaps
         Because of = () => result = maps.Map(typeof(ArtifactImplementation));
 
         It should_return_correct_artifact_type = () => result.ShouldEqual(artifact_type_map.Object);
-    }        
-    
+    }            
 }
