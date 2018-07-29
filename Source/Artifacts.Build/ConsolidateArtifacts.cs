97cfeb2e0e0eb845c3f5e32612f5fdb7f327418e
Einar Ingebrigtsen <einar@dolittle.com>
2018-07-29 13:54:54 +0200

    Make it a tooltask that spawns the CLI tool

--- Source/Artifacts.Build/ConsolidateArtifacts.cs
+++ Source/Artifacts.Build/ConsolidateArtifacts.cs
@@ -2,16 +2,20 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
-namespace Dolittle.SDK.Artifacts.Build
+namespace Dolittle.Artifacts.Build
 {
     /// <summary>
     /// Represents a task for consolidating code artifacts with its configured artifacts in the
     /// mapping file
     /// </summary>
-    public class ConsolidateArtifacts : Task
+    public class ConsolidateArtifacts : ToolTask
     {
         /// <summary>
         /// Gets or sets the base assembly file used. This is typically the output assembly
@@ -20,22 +24,31 @@ namespace Dolittle.SDK.Artifacts.Build
         public ITaskItem AssemblyFile { get; set; }
 
         /// <inheritdoc/>
-        public override bool Execute()
+        protected override string ToolName => "Dolittle.SDK.Artifacts.Tools.dll";
+
+        /// <inheritdoc/>
+        protected override string GenerateFullPathToTool()
         {
-            //while( !System.Diagnostics.Debugger.IsAttached);
-            /*
-                - Read ../artifacts.json
-                - Read ../bounded-context.json
-                - Read ../topology.json
-
-                - Look for new artifacts by finding the ones not in structure
-                - New artifacts - add according to topology
-             */
-
-             Log.LogMessage(MessageImportance.High, $"Consolidate for : {AssemblyFile.ItemSpec}");
-
-            //System.Console.WriteLine($"Consolidate for : {AssemblyFile.ItemSpec}");
-            return true;
+            return "dotnet";
         }
-    }
+
+        /// <inheritdoc/>
+        protected override string GenerateCommandLineCommands()
+        {
+            var assemblyPath = typeof(ConsolidateArtifacts).Assembly.Location;
+            var fileInfo = new FileInfo(assemblyPath);
+            var directory = fileInfo.Directory;
+            var path = Path.Combine(directory.FullName,$"{ToolName}");
+            var targetAssemblyPath = AssemblyFile.GetMetadata("FullPath");
+            var commandLine = $"{path} {targetAssemblyPath}";
+            return commandLine;
+        }
+
+
+        /// <inheritdoc/>
+        protected override MessageImportance StandardOutputLoggingImportance { get; } = MessageImportance.High;
+
+        /// <inheritdoc/>
+        protected override MessageImportance StandardErrorLoggingImportance { get; } = MessageImportance.High;        
+   }
 }
