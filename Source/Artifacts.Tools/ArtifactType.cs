7740abc25fcd7ac0b803dc5f0265e98474ad01d1
Einar Ingebrigtsen <einar@dolittle.com>
2018-07-31 21:07:32 +0200

    Cleanup

--- Source/Artifacts.Tools/ArtifactType.cs
+++ Source/Artifacts.Tools/ArtifactType.cs
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
@@ -5,10 +9,24 @@ using Dolittle.Artifacts.Configuration;
 
 namespace Dolittle.Artifacts.Tools
 {
-    class ArtifactType
+    /// <summary>
+    /// Represents a type of <see cref="Artifact"/>
+    /// </summary>
+    public class ArtifactType
     {
+        /// <summary>
+        /// Gets the type of <see cref="Artifact"/>
+        /// </summary>
         public Type Type { get; set; }
+
+        /// <summary>
+        /// Gets the human friendly type name
+        /// </summary>
         public string TypeName { get; set; } 
+
+        /// <summary>
+        /// Gets the expression for accessing the collection of <see cref="ArtifactDefinition"/> on the configuration object
+        /// </summary>
         public Expression<Func<ArtifactsByTypeDefinition, IEnumerable<ArtifactDefinition>>> TargetPropertyExpression { get; set; }
     }
 }
