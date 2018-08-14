46a81696f3a60a8f2372597da76505e572ae3be4
Einar Ingebrigtsen <einar@dolittle.com>
2018-08-14 15:03:31 +0200

    Taking out dependency on JSON serializer for simplicity

--- Source/Applications.Configuration/BoundedContextConfigurationManager.cs
+++ Source/Applications.Configuration/BoundedContextConfigurationManager.cs
@@ -3,8 +3,10 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 using System.IO;
+using Dolittle.Concepts.Serialization.Json;
 using Dolittle.Execution;
-using Dolittle.Serialization.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Dolittle.Applications.Configuration
 {
@@ -14,18 +16,8 @@ namespace Dolittle.Applications.Configuration
     [Singleton]
     public class BoundedContextConfigurationManager : IBoundedContextConfigurationManager
     {
-        const string _path   = "../bounded-context.json";
-        readonly ISerializer _serializer;
+        const string _path   = "bounded-context.json";
         BoundedContextConfiguration _current;
-
-        /// <summary>
-        /// Initializes a new instance of <see cref="BoundedContextConfigurationManager"/>
-        /// </summary>
-        /// <param name="serializer"></param>
-        public BoundedContextConfigurationManager(ISerializer serializer)
-        {
-            _serializer = serializer;
-        }
 
         /// <inheritdoc/>
         public BoundedContextConfiguration Current 
@@ -44,7 +36,8 @@ namespace Dolittle.Applications.Configuration
             if( !File.Exists(path)) throw new MissingBoundedContextConfiguration();
 
             var json = File.ReadAllText(path);
-            var configuration = _serializer.FromJson<BoundedContextConfiguration>(json);
+            var serializerSettings = GetSerializerSettings();
+            var configuration = JsonConvert.DeserializeObject<BoundedContextConfiguration>(json, serializerSettings);
             return configuration;
         }
 
@@ -52,13 +45,27 @@ namespace Dolittle.Applications.Configuration
         public void Save(BoundedContextConfiguration configuration)
         {
             var path = GetPath();
-            var json = _serializer.ToJson(configuration);
+            var serializerSettings = GetSerializerSettings();
+            var json = JsonConvert.SerializeObject(configuration);
             File.WriteAllText(path, json);
         }
 
         string GetPath()
         {
             return Path.Combine(Directory.GetCurrentDirectory(),_path);
+        }
+
+        JsonSerializerSettings GetSerializerSettings()
+        {
+            var serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented,
+                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+            };
+            serializerSettings.Converters.Add(new ConceptConverter());
+            serializerSettings.Converters.Add(new ConceptDictionaryConverter());
+            return serializerSettings;
         }
     }
 }
