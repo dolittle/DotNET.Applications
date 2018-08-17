53c10d0a92710d28bf61a7ae2ec5dbfec27b1824
Sindre Wilting <sindrewilting@gmail.com>
2018-08-17 14:42:06 +0200

    Added license headers

--- Source/Build/Topology/TopologyConfigurationHandler.cs
+++ Source/Build/Topology/TopologyConfigurationHandler.cs
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 using System;
 using Dolittle.Applications.Configuration;
 using Dolittle.Logging;
@@ -32,7 +36,7 @@ namespace Dolittle.Build.Topology
             var boundedContextConfiguration = _configurationManager.Load();
             return new TopologyBuilder(types, boundedContextConfiguration, logger).Build();
         }
-        
+
         internal void Save(BoundedContextConfiguration config)
         {
             _configurationManager.Save(config);
