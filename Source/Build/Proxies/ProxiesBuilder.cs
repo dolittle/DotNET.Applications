7c58191e00ebf77b03971c48810fa75f49b02105
Sindre Wilting <sindrewilting@gmail.com>
2018-08-22 14:57:30 +0200

    Starting on proxy generation

--- Source/Build/Proxies/ProxiesBuilder.cs
+++ Source/Build/Proxies/ProxiesBuilder.cs
@@ -3,6 +3,9 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 using System;
+using System.Linq;
+using Dolittle.Applications.Configuration;
+using Dolittle.Artifacts.Configuration;
 using Dolittle.Logging;
 
 namespace Dolittle.Build.Proxies
@@ -12,17 +15,26 @@ namespace Dolittle.Build.Proxies
     /// </summary>
      public class ProxiesBuilder
     {
-        readonly Type[] _artifactTypes;
+        readonly Type[] _artifacts;
+        readonly ArtifactsConfiguration _artifactsConfiguration;
+        readonly BoundedContextConfiguration _boundedContextConfiguration;
+        readonly ArtifactType[] _artifactTypes;
         readonly ILogger _logger;
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="artifactsTypes"></param>
+        /// <param name="artifacts">The discovered types of artifacts in the Bounded Context's assemblies</param>
+        /// <param name="artifactsConfiguration"></param>
+        /// <param name="boundedContextConfiguration"></param>
+        /// <param name="artifactTypes">A list of <see cref="ArtifactType"/> which represents the different artifact types</param>
         /// <param name="logger"></param>
-        public ProxiesBuilder(Type[] artifactsTypes, ILogger logger)
+        public ProxiesBuilder(Type[] artifacts, ArtifactsConfiguration artifactsConfiguration, BoundedContextConfiguration boundedContextConfiguration, ArtifactType[] artifactTypes, ILogger logger)
         {
-            _artifactTypes = artifactsTypes;
+            _artifacts = artifacts;
+            _artifactsConfiguration = artifactsConfiguration;
+            _boundedContextConfiguration = boundedContextConfiguration; 
+            _artifactTypes = artifactTypes;
             _logger = logger;
         }
         /// <summary>
@@ -31,11 +43,19 @@ namespace Dolittle.Build.Proxies
         public void Build()
         {
             _logger.Information("Building proxies");
-             var startTime = DateTime.UtcNow;
-
-             var endTime = DateTime.UtcNow;
+            var startTime = DateTime.UtcNow;
+            CreateCommands();
+            var endTime = DateTime.UtcNow;
             var deltaTime = endTime.Subtract(startTime);
             _logger.Information($"Finished proxies build process. (Took {deltaTime.TotalSeconds} seconds)");
+        }
+
+        void CreateCommands()
+        {
+            var commandArtifactType = _artifactTypes.Single(_ => _.TypeName.Equals("command")).Type;
+            var commandArtifacts = _artifacts.Where(_ => commandArtifactType.IsAssignableFrom(_));
+
+            
         }
     }
 }
