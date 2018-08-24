9c9b63fab75a4783c01299645900bd833337b904
Einar Ingebrigtsen <einar@dolittle.com>
2018-08-24 09:56:54 +0200

    Fixing for removed interfaces

--- Specifications/Domain/for_AggregateRootRepositoryFor/given/all_dependencies.cs
+++ Specifications/Domain/for_AggregateRootRepositoryFor/given/all_dependencies.cs
@@ -1,5 +1,4 @@
 ﻿using Dolittle.Runtime.Commands.Coordination;
-using Dolittle.Runtime.Events.Storage;
 using Dolittle.Logging;
 using Machine.Specifications;
 using Moq;
@@ -10,16 +9,12 @@ namespace Dolittle.Domain.for_AggregateRootRepositoryFor.given
     public class all_dependencies
     {
         protected static Mock<ICommandContextManager> command_context_manager;
-        protected static Mock<IEventSourceVersions> event_source_versions;
-        protected static Mock<IEventStore> event_store;
         protected static Mock<IArtifactTypeMap> artifact_type_map;
         protected static Mock<ILogger> logger;
 
         Establish context = () =>
         {
             command_context_manager = new Mock<ICommandContextManager>();
-            event_source_versions = new Mock<IEventSourceVersions>();
-            event_store = new Mock<IEventStore>();
             artifact_type_map = new Mock<IArtifactTypeMap>();
             logger = new Mock<ILogger>();
         };
