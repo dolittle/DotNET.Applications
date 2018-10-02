0950176497ac554d17d8d0f761afeac3f002da19
Sindre Wilting <sindrewilting@gmail.com>
2018-10-02 11:48:47 +0200

    Setup artifact types for specing Topology

---
+++ Specifications/Build/given/ArtifactTypes/Specs_ToStrip_InvalidEvent.cs
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+using Dolittle.Events;
+
+namespace Specs.ToStrip
+{
+    public class InvalidEvent : IEvent
+    {
+        
+    }
+}
