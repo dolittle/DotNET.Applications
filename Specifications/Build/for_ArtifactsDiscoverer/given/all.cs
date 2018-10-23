cf701092bcb3e063708a3fa1e7453715ff1c3272
Sindre Wilting <sindrewilting@gmail.com>
2018-10-23 15:15:22 +0200

    Initial setup for specs

---
+++ Specifications/Build/for_ArtifactsDiscoverer/given/all.cs
@@ -0,0 +1,7 @@
+namespace Dolittle.Build.Specs.for_ArtifactsDiscoverer.given
+{
+    public class all : Dolittle.Build.given.an_ILogger
+    {
+        protected readonly static DolittleArtifactTypes dolittle_artifact_types = new DolittleArtifactTypes();
+    }
+}
---
+++ Specifications/Build/for_ArtifactsDiscoverer/when_constructing_artifacts_discoverer_with_abstract_artifacts.cs
@@ -0,0 +1,7 @@
+namespace Build.for_ArtifactsDiscoverer
+{
+    public class when_constructing_artifacts_discoverer_with_abstract_artifacts
+    {
+        
+    }
+}
