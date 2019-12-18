22cb4900245345ad352b6be03ff0e3578bcb4f50
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-18 08:09:20 +0100

    Static code analysis fixes

--- Source/Applications.Configuration/Topology.cs
+++ Source/Applications.Configuration/Topology.cs
@@ -1,7 +1,6 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Dolittle.Configuration;
@@ -9,15 +8,15 @@ using Dolittle.Configuration;
 namespace Dolittle.Applications.Configuration
 {
     /// <summary>
-    /// Represents the topology of the current <see cref="BoundedContext">bounded context</see>
+    /// Represents the topology of the current <see cref="BoundedContext">bounded context</see>.
     /// </summary>
     public class Topology : IConfigurationObject
     {
         /// <summary>
-        /// Initializes a new instance of <see cref="Topology"/>
+        /// Initializes a new instance of the <see cref="Topology"/> class.
         /// </summary>
-        /// <param name="modules">Collection of <see cref="ModuleDefinition">modules</see>></param>
-        /// <param name="features">Collection of <see cref="FeatureDefinition">features</see></param>
+        /// <param name="modules">Collection of <see cref="ModuleDefinition">modules</see>.</param>
+        /// <param name="features">Collection of <see cref="FeatureDefinition">features</see>.</param>
         public Topology(
             IDictionary<Module, ModuleDefinition> modules,
             IDictionary<Feature, FeatureDefinition> features)
@@ -27,13 +26,13 @@ namespace Dolittle.Applications.Configuration
         }
 
         /// <summary>
-        /// Gets a <see cref="IEnumerable{ModuleDefinition}">collection</see> of <see cref="ModuleDefinition"/>
+        /// Gets a <see cref="IEnumerable{ModuleDefinition}">collection</see> of <see cref="ModuleDefinition"/>.
         /// </summary>
-        public ReadOnlyDictionary<Module, ModuleDefinition> Modules { get; }
+        public ReadOnlyDictionary<Module, ModuleDefinition> Modules { get; }
 
         /// <summary>
-        /// Gets the <see cref="IEnumerable{FeatureDefinition}">features</see> that exists in the root
+        /// Gets the <see cref="IEnumerable{FeatureDefinition}">features</see> that exists in the root.
         /// </summary>
-        public ReadOnlyDictionary<Feature,FeatureDefinition> Features { get; }
+        public ReadOnlyDictionary<Feature, FeatureDefinition> Features { get; }
     }
-}
+}¯
