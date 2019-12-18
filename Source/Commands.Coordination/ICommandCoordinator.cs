fb301eebd6cfb2db6bfdd462f87adab33002437a
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-18 08:59:05 +0100

    Static code analysis fixes

--- Source/Commands.Coordination/ICommandCoordinator.cs
+++ Source/Commands.Coordination/ICommandCoordinator.cs
@@ -1,22 +1,20 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-using Dolittle.Commands;
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Dolittle.Runtime.Commands;
 
 namespace Dolittle.Commands.Coordination
 {
     /// <summary>
-    /// Defines the coordinator commands
+    /// Defines the coordinator of commands.
     /// </summary>
     public interface ICommandCoordinator
     {
         /// <summary>
-        /// Handles a <see cref="ICommand"/> and returns the <see cref="CommandResult"/> from handling it
+        /// Handles a <see cref="ICommand"/> and returns the <see cref="CommandResult"/> from handling it.
         /// </summary>
-        /// <param name="command"><see cref="ICommand"/> to handle</param>
-        /// <returns>The <see cref="CommandResult">result</see></returns>
-        CommandResult   Handle(ICommand command);
+        /// <param name="command"><see cref="ICommand"/> to handle.</param>
+        /// <returns>The <see cref="CommandResult">result</see>.</returns>
+        CommandResult Handle(ICommand command);
     }
 }
