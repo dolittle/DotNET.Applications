c7bdff02afccf0794ff0a6d29a682dd80afbedd8
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-18 09:02:48 +0100

    Static code analysis fixes

--- Source/Commands.Handling/CommandHandlerInvoker.cs
+++ Source/Commands.Handling/CommandHandlerInvoker.cs
@@ -1,7 +1,6 @@
-﻿/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +19,7 @@ namespace Dolittle.Commands.Handling
     /// <summary>
     /// Represents a <see cref="ICommandHandlerInvoker">ICommandHandlerInvoker</see> for handling
     /// command handlers that have methods called Handle() and takes specific <see cref="ICommand">commands</see>
-    /// in as parameters
+    /// in as parameters.
     /// </summary>
     [Singleton]
     public class CommandHandlerInvoker : ICommandHandlerInvoker
@@ -37,13 +36,13 @@ namespace Dolittle.Commands.Handling
         bool _initialized;
 
         /// <summary>
-        /// Initializes a new instance of <see cref="CommandHandlerInvoker">CommandHandlerInvoker</see>
+        /// Initializes a new instance of the <see cref="CommandHandlerInvoker"/> class.
         /// </summary>
-        /// <param name="typeFinder">A <see cref="ITypeFinder"/> to use for discovering <see cref="ICanHandleCommands">command handlers</see></param>
-        /// <param name="container">A <see cref="IContainer"/> to use for getting instances of objects</param>
-        /// <param name="artifactTypeMap"><see cref="IArtifactTypeMap"/> for identifying resources</param>
-        /// <param name="converter"><see cref="ICommandRequestToCommandConverter"/> for converting to actual <see cref="ICommand"/> instances</param>
-        /// <param name="logger"><see cref="ILogger"/> used for logging</param>
+        /// <param name="typeFinder">A <see cref="ITypeFinder"/> to use for discovering <see cref="ICanHandleCommands">command handlers</see>.</param>
+        /// <param name="container">A <see cref="IContainer"/> to use for getting instances of objects.</param>
+        /// <param name="artifactTypeMap"><see cref="IArtifactTypeMap"/> for identifying resources.</param>
+        /// <param name="converter"><see cref="ICommandRequestToCommandConverter"/> for converting to actual <see cref="ICommand"/> instances.</param>
+        /// <param name="logger"><see cref="ILogger"/> used for logging.</param>
         public CommandHandlerInvoker(
             ITypeFinder typeFinder,
             IContainer container,
@@ -60,25 +59,25 @@ namespace Dolittle.Commands.Handling
         }
 
         /// <summary>
-        /// Register a command handler explicitly
+        /// Register a command handler explicitly.
         /// </summary>
-        /// <param name="handlerType"></param>
+        /// <param name="handlerType"><see cref="Type"/> holding the handler.</param>
         /// <remarks>
         /// The registration process will look into the handler and find methods that
-        /// are called Handle() and takes a command as parameter
+        /// are called Handle() and takes a command as parameter.
         /// </remarks>
         public void Register(Type handlerType)
         {
             var handleMethods = handlerType
                 .GetRuntimeMethods()
                 .Where(m => m.IsPublic || !m.IsStatic)
-                .Where(m => m.Name.Equals(HandleMethodName))
+                .Where(m => m.Name.Equals(HandleMethodName, StringComparison.InvariantCulture))
                 .Where(m => m.GetParameters().Length == 1)
-                .Where(m => typeof(ICommand).GetTypeInfo().IsAssignableFrom(m.GetParameters() [0].ParameterType));
+                .Where(m => typeof(ICommand).GetTypeInfo().IsAssignableFrom(m.GetParameters()[0].ParameterType));
 
             handleMethods.ForEach(method =>
             {
-                var commandType = method.GetParameters() [0].ParameterType;
+                var commandType = method.GetParameters()[0].ParameterType;
                 var identifier = _artifactTypeMap.GetArtifactFor(commandType);
                 _commandHandlers[identifier] = method;
             });
@@ -93,9 +92,6 @@ namespace Dolittle.Commands.Handling
 
             if (_commandHandlers.Count == 0) return false;
 
-            var handlerKey = _commandHandlers.Keys.First();
-            var handler = _commandHandlers.First();
-
             if (_commandHandlers.ContainsKey(command.Type))
             {
                 var commandHandlerType = _commandHandlers[command.Type].DeclaringType;
@@ -107,13 +103,14 @@ namespace Dolittle.Commands.Handling
                 _logger.Trace($"Invoke");
                 try
                 {
-                    method.Invoke(commandHandler, new [] { commandInstance });
+                    method.Invoke(commandHandler, new[] { commandInstance });
                 }
                 catch (Exception ex)
                 {
                     _logger.Error(ex, $"Failed invoking command handler '{commandHandlerType.AssemblyQualifiedName}' for command of type '{command.Type}'");
                     return false;
                 }
+
                 return true;
             }
             else
@@ -128,7 +125,7 @@ namespace Dolittle.Commands.Handling
         {
             if (_initialized) return;
 
-            lock(_initializationLock)
+            lock (_initializationLock)
             {
                 if (!_initialized) Initialize();
                 _initialized = true;
