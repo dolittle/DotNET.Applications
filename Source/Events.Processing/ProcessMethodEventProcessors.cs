6e41646b8505cad6c6b48f0431a3d8e984bfe81c
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-20 04:49:44 +0100

    Static code analysis fixes

--- Source/Events.Processing/ProcessMethodEventProcessors.cs
+++ Source/Events.Processing/ProcessMethodEventProcessors.cs
@@ -1,42 +1,40 @@
-﻿/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Dolittle.Artifacts;
 using Dolittle.DependencyInversion;
+using Dolittle.Lifecycle;
+using Dolittle.Logging;
+using Dolittle.PropertyBags;
+using Dolittle.Runtime.Events.Processing;
 using Dolittle.Time;
 using Dolittle.Types;
-using Dolittle.Runtime.Events.Processing;
-using Dolittle.Logging;
-using Dolittle.Artifacts;
-using Dolittle.PropertyBags;
-using Dolittle.Lifecycle;
-using Dolittle.Collections;
 
 namespace Dolittle.Events.Processing
 {
     /// <summary>
-    /// Represents an implementation of <see cref="IKnowAboutEventProcessors"/> for 
+    /// Represents an implementation of <see cref="IKnowAboutEventProcessors"/> for.
     /// <see cref="IEventProcessor">event processors</see> in the currently running process.
     /// </summary>
     /// <remarks>
     /// The <see cref="IEventProcessor">processors</see> this implementation deals with is your
-    /// .NET based and discovered <see cref="IEventProcessor">processors</see>
+    /// .NET based and discovered <see cref="IEventProcessor">processors</see>.
     /// </remarks>
     [Singleton]
     public class ProcessMethodEventProcessors : IKnowAboutEventProcessors
     {
         /// <summary>
-        /// The separator used in the <see cref="EventProcessorId"/> between the type and the event it handles
+        /// The separator used in the <see cref="EventProcessorId"/> between the type and the event it handles.
         /// </summary>
         public const string IdentifierSeparator = "|";
 
         /// <summary>
-        /// Name of method that any event subscriber needs to be called in order to be recognized by the convention
+        /// Name of method that any event subscriber needs to be called in order to be recognized by the convention.
         /// </summary>
         public const string ProcessMethodName = "Process";
 
@@ -48,16 +46,15 @@ namespace Dolittle.Events.Processing
         readonly IObjectFactory _objectFactory;
         readonly ILogger _logger;
 
-
         /// <summary>
-        /// Initializes a new instance of <see cref="ProcessMethodEventProcessors"/>
+        /// Initializes a new instance of the <see cref="ProcessMethodEventProcessors"/> class.
         /// </summary>
-        /// <param name="artifactTypeMap"><see cref="IArtifactTypeMap"/> for identifying <see cref="IEvent">events</see> </param>
-        /// <param name="objectFactory"><see cref="IObjectFactory"/> for going between <see cref="PropertyBag"/> and instances of types</param>
-        /// <param name="processors"><see cref="IImplementationsOf{ICanProcessEvents}"/> for discovering implementations of <see cref="ICanProcessEvents"/></param>
-        /// <param name="container"><see cref="IContainer"/> for the implementation <see cref="ProcessMethodEventProcessor"/> when acquiring instances of implementations of <see cref="ICanProcessEvents"/></param>
-        /// <param name="systemClock"><see cref="ISystemClock"/> for timing <see cref="IEventProcessors"/></param>
-        /// <param name="logger"><see cref="ILogger"/> for logging</param>
+        /// <param name="artifactTypeMap"><see cref="IArtifactTypeMap"/> for identifying <see cref="IEvent">events</see>.</param>
+        /// <param name="objectFactory"><see cref="IObjectFactory"/> for going between <see cref="PropertyBag"/> and instances of types.</param>
+        /// <param name="processors"><see cref="IImplementationsOf{ICanProcessEvents}"/> for discovering implementations of <see cref="ICanProcessEvents"/>.</param>
+        /// <param name="container"><see cref="IContainer"/> for the implementation <see cref="ProcessMethodEventProcessor"/> when acquiring instances of implementations of <see cref="ICanProcessEvents"/>.</param>
+        /// <param name="systemClock"><see cref="ISystemClock"/> for timing <see cref="IEventProcessors"/>.</param>
+        /// <param name="logger"><see cref="ILogger"/> for logging.</param>
         public ProcessMethodEventProcessors(
             IArtifactTypeMap artifactTypeMap,
             IObjectFactory objectFactory,
@@ -86,9 +83,8 @@ namespace Dolittle.Events.Processing
             return _eventProcessors.GetEnumerator();
         }
 
-
         /// <summary>
-        /// Populates from in process event processors through discovery
+        /// Populates from in process event processors through discovery.
         /// </summary>
         public void Populate()
         {
@@ -96,8 +92,7 @@ namespace Dolittle.Events.Processing
             {
                 _logger.Trace($"Processor '{processor.AssemblyQualifiedName}'");
                 processor.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly).ToList().ForEach(_ => _logger.Trace($"{_.Name} {_.EventProcessorId()}"));
-                var methods = processor.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly).Where(m => m.EventProcessorId().Value != Guid.Empty).ToList();
-                foreach (var method in methods)
+                foreach (var method in processor.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly).Where(m => m.EventProcessorId().Value != Guid.Empty).ToList())
                 {
                     _logger.Trace($"Method found '{method} {method:EventProcessorId()}'");
 
@@ -109,11 +104,11 @@ namespace Dolittle.Events.Processing
 
                     var processMethodEventProcessor = new ProcessMethodEventProcessor(
                                                             _objectFactory,
-                                                            _container, 
-                                                            eventProcessorId, 
+                                                            _container,
+                                                            eventProcessorId,
                                                             eventIdentifier,
                                                             parameterType,
-                                                            method, 
+                                                            method,
                                                             _logger);
 
                     _eventProcessors.Add(processMethodEventProcessor);
