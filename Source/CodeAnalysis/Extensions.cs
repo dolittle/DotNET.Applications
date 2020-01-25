664171e848c60f8a879b05e5614af97c7ad9bca4
Einar Ingebrigtsen <einar@dolittle.com>
2020-01-25 08:33:25 +0100

    Adding an extension for checking for interface implementation

--- Source/CodeAnalysis/Extensions.cs
+++ Source/CodeAnalysis/Extensions.cs
@@ -15,21 +15,28 @@ namespace Dolittle.SDK.CodeAnalysis
     public static class Extensions
     {
         /// <summary>
-        /// Check if a <see cref="ClassDeclarationSyntax"/> implements an event.
+        /// Check if a <see cref="ClassDeclarationSyntax"/> implements a specific interface.
         /// </summary>
+        /// <typeparam name="T">The type of interface to check for.</typeparam>
         /// <param name="classDeclaration"><see cref="ClassDeclarationSyntax"/> to check.</param>
         /// <param name="model"><see cref="SemanticModel"/> to use.</param>
         /// <returns>true if it inherits, false if not.</returns>
-        public static bool ImplementsAnEvent(this ClassDeclarationSyntax classDeclaration, SemanticModel model)
+        public static bool ImplementsInterfaceOf<T>(this ClassDeclarationSyntax classDeclaration, SemanticModel model)
+            where T : class
         {
+            var @interface = typeof(T);
+            if (!@interface.IsInterface)
+            {
+                throw new TypeMustBeAnInterface(@interface);
+            }
+
             var classSymbol = model.GetDeclaredSymbol(classDeclaration);
-            var interfaces = classDeclaration
-                .SyntaxTree
-                .GetRoot()
-                .DescendantNodes()
-                .OfType<InterfaceDeclarationSyntax>();
-            return classSymbol.BaseType.ContainingNamespace.Name.Equals("Dolittle.Events", StringComparison.InvariantCulture) &&
-                classSymbol.BaseType.Name.EndsWith("IEvent", StringComparison.InvariantCulture);
+            var interfaceDeclaration = classSymbol.AllInterfaces.FirstOrDefault(_ =>
+                _.ContainingAssembly.Name.Equals(@interface.Assembly.GetName().Name, StringComparison.InvariantCulture) &&
+                _.ContainingNamespace.ToDisplayString().Equals(@interface.Namespace, StringComparison.InvariantCulture) &&
+                _.Name.Equals(@interface.Name, StringComparison.InvariantCulture));
+
+            return interfaceDeclaration != default;
         }
     }
 }
