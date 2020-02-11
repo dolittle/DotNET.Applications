73db3d98db64e0308a60a478bf310655ab9542aa
Einar Ingebrigtsen <einar@dolittle.com>
2020-02-11 10:41:20 +0100

    Getting things up and running

--- Samples/EventSourcing/MyAggregate.cs
+++ Samples/EventSourcing/MyAggregate.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Dolittle.Domain;
+using Dolittle.Events;
 using Dolittle.Runtime.Events;
 
 namespace EventSourcing
