d4653c7ad83939d93a474c3241107f1437ad7913
Sindre Wilting <sindrewilting@gmail.com>
2020-03-26 11:09:10 +0100

    Define named policies

--- Source/Events.Processing/StartEventProcessorPolicy.cs
+++ Source/Events.Processing/StartEventProcessorPolicy.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Dolittle. All rights reserved.
+// Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
@@ -6,34 +6,35 @@ using Dolittle.Logging;
 using Dolittle.Resilience;
 using Polly;
 
-namespace Dolittle.Events.Handling
+namespace Dolittle.Events.Processing
 {
     /// <summary>
-    /// Defines the policy for <see cref="EventHandlerProcessorPolicy"/>.
+    /// Defines the policy for starting an event processor.
     /// </summary>
-    public class EventHandlerProcessorPolicy : IDefineAsyncPolicyForType
+    public class StartEventProcessorPolicy : IDefineNamedAsyncPolicy
     {
         readonly ILogger _logger;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="EventHandlerProcessorPolicy"/> class.
+        /// Initializes a new instance of the <see cref="StartEventProcessorPolicy"/> class.
         /// </summary>
         /// <param name="logger"><see cref="ILogger"/> for logging.</param>
-        public EventHandlerProcessorPolicy(ILogger logger)
+        public StartEventProcessorPolicy(ILogger logger)
         {
             _logger = logger;
+            Name = GetType().Name;
         }
 
         /// <inheritdoc/>
-        public Type Type => typeof(EventHandlerProcessor);
+        public string Name { get; }
 
         /// <inheritdoc/>
         public Polly.IAsyncPolicy Define() =>
             Polly.Policy.Handle<Exception>(_ =>
                 {
-                    _logger.Warning($"Failed to write event processing result to runtime : {_.Message}");
+                    _logger.Warning($"Failed to start event processor : {_.Message}");
                     return true;
                 })
-                .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(1));
+                .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(5));
     }
 }
---
+++ Source/Events.Processing/WriteEventProcessingResponsePolicy.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Dolittle.Logging;
+using Dolittle.Resilience;
+using Polly;
+
+namespace Dolittle.Events.Processing
+{
+    /// <summary>
+    /// Defines the policy for writing event processing response to runtime.
+    /// </summary>
+    public class WriteEventProcessingResponsePolicy : IDefineNamedAsyncPolicy
+    {
+        readonly ILogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WriteEventProcessingResponsePolicy"/> class.
+        /// </summary>
+        /// <param name="logger"><see cref="ILogger"/> for logging.</param>
+        public WriteEventProcessingResponsePolicy(ILogger logger)
+        {
+            _logger = logger;
+            Name = GetType().Name;
+        }
+
+        /// <inheritdoc/>
+        public string Name { get; }
+
+        /// <inheritdoc/>
+        public Polly.IAsyncPolicy Define() =>
+            Polly.Policy.Handle<Exception>(_ =>
+                {
+                    _logger.Warning($"Failed to write event processing response to runtime : {_.Message}");
+                    return true;
+                })
+                .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(1));
+    }
+}
