fcfc1604c2240185134ab5b36b1b05b10173917c
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-05-07 16:03:39 +0200

    Remove usings in specs for Validation

--- Specifications/Validation/for_RuleBuilderExtensions/FakePropertyValidatorWithDynamicState.cs
+++ Specifications/Validation/for_RuleBuilderExtensions/FakePropertyValidatorWithDynamicState.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Linq.Expressions;
-using Dolittle.Validation;
 using FluentValidation.Validators;
 
 namespace Dolittle.Validation.for_RuleBuilderExtensions
--- Specifications/Validation/for_RuleBuilderExtensions/when_adding_dynamic_state_to_validator_not_capable_of_dynamic_state.cs
+++ Specifications/Validation/for_RuleBuilderExtensions/when_adding_dynamic_state_to_validator_not_capable_of_dynamic_state.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Dolittle.Validation;
 using FluentValidation;
 using Machine.Specifications;
 using Moq;
