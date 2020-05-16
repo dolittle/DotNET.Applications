548879935143e8b960dfa7d884f5e8a83d555842
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-05-16 13:49:29 +0200

    Introduce ExternalEventContext which represents the contect for a CommittedExternalEvent.

--- Source/Events/EventContext.cs
+++ Source/Events/EventContext.cs
@@ -7,7 +7,7 @@ using Dolittle.Execution;
 namespace Dolittle.Events
 {
     /// <summary>
-    /// Reoresents the context in which an event occured in.
+    /// Represents the context in which an event occured in.
     /// </summary>
     public class EventContext
     {
@@ -23,12 +23,35 @@ namespace Dolittle.Events
             EventSourceId eventSourceId,
             DateTimeOffset occurred,
             ExecutionContext executionContext)
+            : this(
+                sequenceNumber,
+                eventSourceId,
+                occurred,
+                executionContext,
+                new EventIdentifier(executionContext.Microservice, executionContext.Tenant, sequenceNumber))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventContext"/> class.
+        /// </summary>
+        /// <param name="sequenceNumber">The <see cref="EventLogSequenceNumber">sequence number</see> that uniquely identifies the event in the event log which it was committed.</param>
+        /// <param name="eventSourceId">The <see cref="EventSourceId"/> that the event was committed to.</param>
+        /// <param name="occurred">The <see cref="DateTimeOffset"/> when the event was committed to the <see cref="IEventStore"/>.</param>
+        /// <param name="executionContext">The <see cref="ExecutionContext"/> in which the event was committed to the <see cref="IEventStore"/>.</param>
+        /// <param name="uniqueIdentifier">The <see cref="EventIdentifier"/> that uniquely identifies the event.</param>
+        protected EventContext(
+            EventLogSequenceNumber sequenceNumber,
+            EventSourceId eventSourceId,
+            DateTimeOffset occurred,
+            ExecutionContext executionContext,
+            EventIdentifier uniqueIdentifier)
         {
             SequenceNumber = sequenceNumber;
             EventSourceId = eventSourceId;
             Occurred = occurred;
             ExecutionContext = executionContext;
-            UniqueIdentifier = new EventIdentifier(ExecutionContext.Microservice, ExecutionContext.Tenant, SequenceNumber);
+            UniqueIdentifier = uniqueIdentifier;
         }
 
         /// <summary>
