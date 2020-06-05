fb483871671e53b3e95bfeafac5d8057e41c6ccb
Joel Hoisko <joel.hoisko@gmail.com>
2020-06-05 14:04:22 +0200

    Add more tests for eventhandlerregistry

--- Specifications/Events.Handling/for_EventHandlerRegistry/when_registering_an_event_handler.cs
+++ Specifications/Events.Handling/for_EventHandlerRegistry/when_registering_an_event_handler.cs
@@ -14,31 +14,43 @@ namespace Dolittle.Events.Handling.for_EventHandlerRegistry
 {
     public class when_registering_an_event_handler
     {
+        static Mock<IAsyncPolicyFor<EventHandlerRegistry>> _policy;
         static Mock<IEventProcessingCompletion> _completion;
 
         static Mock<IEventHandler<IEvent>> _handler;
+
+        static Mock<IEventHandlerProcessors> _processors;
+
+        static Mock<IEventProcessor> _processor;
 
         static EventHandlerRegistry registry;
 
         Establish context = () =>
         {
-            var policy = new Mock<IAsyncPolicyFor<EventHandlerRegistry>>();
+            _policy = new Mock<IAsyncPolicyFor<EventHandlerRegistry>>();
             _completion = new Mock<IEventProcessingCompletion>();
             _handler = new Mock<IEventHandler<IEvent>>();
+            _processor = new Mock<IEventProcessor>();
 
-            var processors = new Mock<IEventHandlerProcessors>();
-            processors.Setup(_ => _.GetFor<IEvent>(
+            _processors = new Mock<IEventHandlerProcessors>();
+            _processors.Setup(_ => _.GetFor<IEvent>(
                 MIt.IsAny<EventHandlerId>(),
                 MIt.IsAny<ScopeId>(),
                 MIt.IsAny<bool>(),
                 MIt.IsAny<IEventHandler<IEvent>>()))
-                .Returns(new Mock<IEventProcessor>().Object);
-            registry = new EventHandlerRegistry(processors.Object, policy.Object, _completion.Object);
+                .Returns(_processor.Object);
+            registry = new EventHandlerRegistry(_processors.Object, _policy.Object, _completion.Object);
         };
 
         Because of = () => registry.Register<IEvent>(Guid.Empty, Guid.Empty, false, _handler.Object);
 
         It should_register_the_handler_to_the_completion = () =>
             _completion.Verify(_ => _.RegisterHandler(Guid.Empty, _handler.Object.HandledEventTypes), Times.Once());
+
+        It should_get_a_processor_from_processors = () =>
+            _processors.Verify(_ => _.GetFor(Guid.Empty, Guid.Empty, false, _handler.Object), Times.Once());
+
+        It should_register_a_processor_with_policy = () =>
+            _processor.Verify(_ => _.RegisterAndHandleForeverWithPolicy(_policy.Object, default), Times.Once());
     }
 }
