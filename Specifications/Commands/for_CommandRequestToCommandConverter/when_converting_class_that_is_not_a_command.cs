e3409ad2be44305452bd55ee425eab686791a10e
Sindre Wilting <sindrewilting@gmail.com>
2020-06-10 09:47:44 +0200

    Add spec for converting a command request that is not a command type

---
+++ Specifications/Commands/for_CommandRequestToCommandConverter/when_converting_class_that_is_not_a_command.cs
@@ -0,0 +1,38 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using Dolittle.Artifacts;
+using Dolittle.Execution;
+using Machine.Specifications;
+using It = Machine.Specifications.It;
+
+namespace Dolittle.Commands.for_CommandRequestToCommandConverter
+{
+    public class when_converting_class_that_is_not_a_command : given.a_converter
+    {
+        static CorrelationId correlation_id;
+        static Artifact identifier;
+        static CommandRequest request;
+        static Exception exception;
+
+        Establish context = () =>
+        {
+            correlation_id = CorrelationId.New();
+            identifier = Artifact.New();
+
+            request = new CommandRequest(correlation_id, identifier.Id, identifier.Generation, new Dictionary<string, object>());
+
+            artifact_type_map.Setup(_ => _.GetTypeFor(identifier)).Returns(typeof(complex_type));
+        };
+
+        Because of = () =>
+        {
+            while (!System.Diagnostics.Debugger.IsAttached) System.Threading.Thread.Sleep(50);
+            exception = Catch.Exception(() => converter.Convert(request));
+        };
+
+        It should_fail_because_artifact_is_not_a_command = () => exception.ShouldBeOfExactType<ArtifactIsNotCommand>();
+    }
+}
