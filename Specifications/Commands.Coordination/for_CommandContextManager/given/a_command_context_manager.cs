e32cc6340f8388a14a37c86153497e2b074c79fa
Sindre Wilting <sindrewilting@gmail.com>
2020-06-10 09:31:01 +0200

    Fix Command.Coordination specs

--- Specifications/Commands.Coordination/for_CommandContextManager/given/a_command_context_manager.cs
+++ Specifications/Commands.Coordination/for_CommandContextManager/given/a_command_context_manager.cs
@@ -1,10 +1,14 @@
 ﻿// Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Globalization;
 using Dolittle.Events;
 using Dolittle.Events.Handling;
 using Dolittle.Execution;
 using Dolittle.Logging;
+using Dolittle.Security;
+using Dolittle.Tenancy;
 using Machine.Specifications;
 using Moq;
 
@@ -15,15 +19,28 @@ namespace Dolittle.Commands.Coordination.for_CommandContextManager.given
         protected static CommandContextManager manager;
         protected static Mock<IEventStore> event_store;
         protected static Mock<IExecutionContextManager> execution_context_manager;
+        protected static ExecutionContext execution_context;
         protected static Mock<IEventProcessingCompletion> event_processing_completion;
+        protected static TenantId tenant;
         protected static CommandContextFactory factory;
         protected static Mock<ILogger> logger;
 
         Establish context = () =>
         {
             CommandContextManager.ResetContext();
+            tenant = Guid.Parse("60574bab-60a8-4c75-9940-e5f53c79df73");
             event_store = new Mock<IEventStore>();
             execution_context_manager = new Mock<IExecutionContextManager>();
+            execution_context = new ExecutionContext(
+                    Guid.Parse("2e1224b5-2534-4a4d-bb22-12013a690288"),
+                    tenant,
+                    new Versioning.Version(1, 2, 3, 4, ""),
+                    Execution.Environment.Development,
+                    Guid.Parse("7280bdc8-72d0-48bd-8ea3-6a1f6ffa1bd2"),
+                    Claims.Empty,
+                    CultureInfo.InvariantCulture);
+            execution_context_manager
+                .SetupGet(_ => _.Current).Returns(() => execution_context);
             event_processing_completion = new Mock<IEventProcessingCompletion>();
             logger = new Mock<ILogger>();
 
